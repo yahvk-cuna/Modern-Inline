b0532874f2fb7a1ec1e40de4f4f5d16841e6ebb3
yahvk <71e6fd52@gmail.com>
2025-09-08 23:11:35 +0800

    support 1.21.1 & support MUI 3.12

---
+++ neoforge/src/main/java/moe/yahvk/moderninline/neoforge/ModerninlineNeoForge.java
@@ -0,0 +1,12 @@
+package moe.yahvk.moderninline.neoforge;
+
+import moe.yahvk.moderninline.Moderninline;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Moderninline.MOD_ID)
+public final class ModerninlineNeoForge {
+    public ModerninlineNeoForge() {
+        // Run our common setup.
+        Moderninline.init();
+    }
+}
